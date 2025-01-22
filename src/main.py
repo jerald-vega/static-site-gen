ea2be8259867e87c9e4a99fa274bceb054b9bfd6
Jerald Vega <xfcgjyfc@gmail.com>
2025-01-21 21:55:40 -0300

    start project

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+print("hello world")
+
+def main():
+    t_node_ej = TextNode("This is a text node", TextType.NORMAL, "https://www.google.com")
+    print(t_node_ej)
+
+main()
